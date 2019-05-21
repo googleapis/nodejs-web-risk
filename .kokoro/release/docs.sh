35bfd69d89bcb40060334b2e0e12eeca708201da
Benjamin Coe <bencoe@google.com>
2019-05-21 12:28:16 -0700

    build: run in appropriate directory

--- .kokoro/release/docs.sh
+++ .kokoro/release/docs.sh
@@ -21,6 +21,7 @@ if [[ -z "$CREDENTIALS" ]]; then
   # if CREDENTIALS are explicitly set, assume we're testing locally
   # and don't set NPM_CONFIG_PREFIX.
   export NPM_CONFIG_PREFIX=/home/node/.npm-global
+  cd $(dirname $0)/../..
 fi
 npm install
 npm run docs
