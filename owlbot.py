365335b477cb5160c6339dd5ac2f244e6bf62a7b
Jeffrey Rennie <rennie@google.com>
2021-03-11 19:53:37 -0800

    chore: another experiment with a staging dir

--- owlbot.py
+++ owlbot.py
@@ -17,24 +17,21 @@ import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.node as node
 import logging
+import pathlib
+import shutil
 
 logging.basicConfig(level=logging.DEBUG)
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
+# default version must come last.
+versions = ['v1beta1', 'v1']
+default_version = versions[-1]
 
-# run the gapic generator
-gapic = gcp.GAPICBazel()
-versions = ['v1', 'v1beta1']
-default_version = 'v1'
-
-# Rearrange the default version to the last item in the array, to generate appropriate system-test
-order_versions = versions.copy()
-order_versions.append(order_versions.pop(
-    order_versions.index(default_version)))
-
-for version in order_versions:
-    library = gapic.node_library('webrisk', version)
-    s.copy(library, excludes=['package.json', 'README.md'])
+staging = pathlib.Path('owl-bot-staging')
+if staging.is_dir():
+    for version in versions:
+        library = f'owl-bot-staging/webrisk-{version}-nodejs'
+        s.copy(library, excludes=['package.json', 'README.md'])
+    shutil.rmtree(staging)
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
@@ -42,4 +39,4 @@ templates = common_templates.node_library(
     source_location='build/src', versions=versions, default_version=default_version)
 s.copy(templates, excludes=['.nycrc'])
 
-node.postprocess_gapic_library()
+node.postprocess_gapic_library_hermetic()
